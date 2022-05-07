ccdb3355a22daac052c7bdea7de8ca4cda0381dd
unknown <vedstandard@runbox.com>
2022-05-07 04:34:47 +0200

    binding

--- app/src/main/java/com/example/androidfundamentalskotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentalskotlin/MainActivity.kt
@@ -2,12 +2,30 @@ package com.example.androidfundamentalskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import com.example.androidfundamentalskotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.buttonApply.setOnClickListener{
+            val firstName = binding.etFirstName.text.toString()
+            val lastName = binding.etLastName.text.toString()
+            val birthDate = binding.etBirthDate.text.toString()
+            val country = binding.etCountry.text.toString()
+
+            Log.d("MainActivity", "$firstName $lastName born on $birthDate in $country")
+        }
 
     }
 }
+
