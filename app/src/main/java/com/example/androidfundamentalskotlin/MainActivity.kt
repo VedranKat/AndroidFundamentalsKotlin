812be08242ffc1eb9805287b364deaff57e443ae
unknown <vedstandard@runbox.com>
2022-05-09 23:55:13 +0200

    appbar practice

--- app/src/main/java/com/example/androidfundamentalskotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentalskotlin/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.androidfundamentalskotlin
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.RadioButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.androidfundamentalskotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,21 +22,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnOrder.setOnClickListener(){
-            val checkedMeatRadioButtonId = binding.rgMeat.checkedRadioButtonId
-            val meat = findViewById<RadioButton>(checkedMeatRadioButtonId)
-            val cheese = binding.cbCheese.isChecked
-            val onions = binding.cbOnions.isChecked
-            val salad = binding.cbSalad.isChecked
-            val orderString = "You order a burger with: \n" +
-                    "${meat.text}" +
-                    (if(cheese)"\nCheese" else "") +
-                    (if(onions)"\nOnions" else "") +
-                    (if(salad)"\nSalad" else "")
 
-            binding.tvOrder.text = orderString
 
-        }
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.miAddContact -> Toast.makeText(this, "You Clicked On Add Contact", Toast.LENGTH_SHORT).show()
+            R.id.miFavorites -> Toast.makeText(this, "You Clicked On Favaorites", Toast.LENGTH_SHORT).show()
+            R.id.miSettings -> Toast.makeText(this, "You Clicked On Settings", Toast.LENGTH_SHORT).show()
+            R.id.miClose -> finish()
+            R.id.miFeedback -> Toast.makeText(this, "You Clicked On Feedback", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+
 }
