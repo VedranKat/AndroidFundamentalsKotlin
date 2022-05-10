9bb1d81f90a4891c25d2857d70507863e6957d2e
unknown <vedstandard@runbox.com>
2022-05-10 22:51:23 +0200

    basic rview

--- app/src/main/java/com/example/androidfundamentalskotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentalskotlin/MainActivity.kt
@@ -8,9 +8,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.RadioButton
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidfundamentalskotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,25 +24,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var todoList = mutableListOf(
+            Todo("Follow AndroidDevs", false),
+            Todo("Learn about RecyclerView", true),
+            Todo("Tutorial is deprecated", true),
+            Todo("Finish AI project", true),
+            Todo("Study for test in 2 days", true)
+        )
 
+        val adapter = TodoAdapter(todoList)
+        binding.rvTodo.adapter = adapter
+        binding.rvTodo.layoutManager = LinearLayoutManager(this)
 
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.app_bar_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.miAddContact -> Toast.makeText(this, "You Clicked On Add Contact", Toast.LENGTH_SHORT).show()
-            R.id.miFavorites -> Toast.makeText(this, "You Clicked On Favaorites", Toast.LENGTH_SHORT).show()
-            R.id.miSettings -> Toast.makeText(this, "You Clicked On Settings", Toast.LENGTH_SHORT).show()
-            R.id.miClose -> finish()
-            R.id.miFeedback -> Toast.makeText(this, "You Clicked On Feedback", Toast.LENGTH_SHORT).show()
+        binding.btnTodo.setOnClickListener(){
+            val title = binding.etTodo.text.toString()
+            val todo = Todo(title, false)
+            todoList.add(todo)
+            // adapter.notifyDataSetChanged() - works too but updates the whole Rview instead of a single item
+            adapter.notifyItemInserted(todoList.size - 1)
         }
-        return true
+
     }
 
 }
